a2f1d6a55b448856bf937181fd2f545de827e370
ErnstVC <ernst.vivasc@udea.edu.co>
2025-07-13 17:15:28 -0500

    listo CSV

--- Controlador.py
+++ Controlador.py
@@ -1,22 +1,35 @@
+
 import matplotlib.pyplot as plt
-from PyQt5.QtWidgets import QFileDialog
-#importo las clases de los archivos modelo y vista
+from PyQt5.QtWidgets import QFileDialog, QTableWidgetItem
 from Modelo import *
 from vistaa import *
 
 class Coordinador:
-    def __init__(self, vista, modelo): #el controlador recibe todo de la vista y el modelo
-        self.__mivista = vista
-        self.__mimodelo = modelo
+    def __init__(self, vista, modelo):
+        self.vista = vista
+        self.modelo = modelo
         self.conectar_eventos()
 
     def conectar_eventos(self):
-        self.vista.btn_cargar.clicked.connect(self.cargar_csv)
+        self.vista.btn_cargar.clicked.connect(self.seleccionar_archivo)
         self.vista.btn_graficar.clicked.connect(self.graficar)
 
-    def cargar_csv(self,ruta):
-       return self.__mimodelo.cargar_csv(ruta) # como en el modelo se define esta función, aquí se llama
-     
+    def seleccionar_archivo(self):
+        ruta, _ = QFileDialog.getOpenFileName(self.vista, "Selecciona un archivo CSV", "", "CSV files (*.csv)")
+        if ruta:
+            self.cargar_csv(ruta)
+            columnas = self.modelo.obtener_columnas()
+            self.vista.combo_x.clear()
+            self.vista.combo_y.clear()
+            self.vista.combo_x.addItems(columnas)
+            self.vista.combo_y.addItems(columnas)
+            self.mostrar_tabla()
+
+    def cargar_csv(self, ruta):
+        self.modelo.cargar_csv(ruta)
+        nombre = os.path.basename(ruta)
+        registro = RegistroArchivo("csv", nombre, ruta, coleccion_archivos)
+        registro.guardar()
 
     def mostrar_tabla(self):
         df = self.modelo.obtener_datos()
